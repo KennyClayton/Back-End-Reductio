8b149680e165f447c98f4cebec0ec9b390f65145
Kenny Clayton <kenny.d.clayton@gmail.com>
2023-08-27 12:10:59 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string greeting = "Welcome!";
+Console.WriteLine(greeting);
+Console.WriteLine("Please choose a product: ");
+
+string response = Console.ReadLine();
+
+Console.WriteLine($"You chose: {response}");
