868f413f15dddfde28915e8e543d42450d2f5408
Kenny Clayton <kenny.d.clayton@gmail.com>
2023-08-29 11:49:19 -0400

    Reductio Final push; functional

--- Product.cs
+++ Product.cs
@@ -1,12 +1,35 @@
 using System.Diagnostics;
+using System;
 
-public class Product 
+
+public class Product
 {
     public string Name { get; set; }
     public decimal Price { get; set; }
     public bool Available { get; set; }
     public int ProductTypeId { get; set; }
     public ProductType Type { get; set; } //here, Type is not an integer or boolean or string....it's a ProductType
+    public DateTime DateStocked { get; set; }
+
+    //* BELOW IS THE FIRST PILLAR OF OOP - Abstraction "...keeping all of those details in the Product class itself, so that all the code that uses the class needs to know is that there is a property on the class DaysOnShelf that can be accessed."
+    public int DaysOnShelf // look at this as another property of each product...only when we ACCESS this data we can only retrieve (GET) it using DateStocked. We cannot SET this property to anything directly. We only need to set the value of the DateStocked property and then this DaysOnShelf property will piggyback off the data inside DateStocked property.
+    {
+        get
+        {
+            TimeSpan timeOnShelf = DateTime.Now - DateStocked;
+            return timeOnShelf.Days;
+        }
+    }
+    
+    // look at this as another property of each product...only when we ACCESS this data we can only retrieve (GET) it using DateStocked. We cannot SET this property to anything directly. We only need to set the value of the DateStocked property and then this DaysOnShelf property will piggyback off the data inside DateStocked property.
+    public int SecondsOnShelf 
+    {
+        get
+        {
+            TimeSpan timeOnShelf = DateTime.Now - DateStocked;
+            return (int)timeOnShelf.TotalSeconds;
+        }
+    }
 }
 
 public class ProductType
@@ -15,9 +38,11 @@ public class ProductType
     public string Name { get; set; }
 }
 
+
+
 //^ We created a class called Plant. What does a class do?
-    // A class looks like an object, but it's just a BLUEPRINT for creating objetcs.
-    // When I create this Plant class, I can then create Plant class objects (plant instances) that MUST adhere to the above blueprint. That is, every new INSTANCE of a plant that I create MUST have only these three properties above: name, price, available and product type Id. And each of those properties will only be string data type for the Name, decimal data type for the Price, boolean data type for Available and integer data type for the ProductTypeId...
-    // This Plant class is the guide, the template, the blueprint for any plant "object" or "instance" I may want to create in the future. 
-    // So, where do we create the plant objects/instances then? We create them in our Program file at the top of that file. The file gets read from top to bottom when being executed so it needs that code (those objects) first
-    // So, again, as ChatGPT puts it: "Classes in C# define the structure (fields/properties and methods) that objects will have."
+// A class looks like an object, but it's just a BLUEPRINT for creating objetcs.
+// When I create this Plant class, I can then create Plant class objects (plant instances) that MUST adhere to the above blueprint. That is, every new INSTANCE of a plant that I create MUST have only these three properties above: name, price, available and product type Id. And each of those properties will only be string data type for the Name, decimal data type for the Price, boolean data type for Available and integer data type for the ProductTypeId...
+// This Plant class is the guide, the template, the blueprint for any plant "object" or "instance" I may want to create in the future. 
+// So, where do we create the plant objects/instances then? We create them in our Program file at the top of that file. The file gets read from top to bottom when being executed so it needs that code (those objects) first
+// So, again, as ChatGPT puts it: "Classes in C# define the structure (fields/properties and methods) that objects will have."
--- Program.cs
+++ Program.cs
@@ -11,56 +11,64 @@ List<Product> products = new List<Product>() //here is where we declare a new Li
         Name = "Invisibility Cloak",
         Price = 27.00M,
         Available = true,
-        ProductTypeId = 1
+        ProductTypeId = 1,
+        DateStocked = new (2023, 6, 15)
     },
     new Product()
     {
         Name = "Dress Robes",
         Price = 39.00M,
         Available = true,
-        ProductTypeId = 1
+        ProductTypeId = 1,
+        DateStocked = new (2023, 7, 15)
     },
     new Product()
     {
         Name = "Polyjuice Potion",
         Price = 27.00M,
         Available = true,
-        ProductTypeId = 2
+        ProductTypeId = 2,
+        DateStocked = new (2023, 8, 15)
     },
     new Product()
     {
         Name = "Wideye Potion",
         Price = 36.00M,
         Available = true,
-        ProductTypeId = 2
+        ProductTypeId = 2,
+        DateStocked = new (2023, 1, 15)
     },
     new Product()
     {
         Name = "Deluminator",
         Price = 600.00M,
         Available = true,
-        ProductTypeId = 3
+        ProductTypeId = 3,
+        DateStocked = new (2023, 2, 15)
     },
     new Product()
     {
         Name = "Talking Mirror",
         Price = 527.00M,
         Available = true,
-        ProductTypeId = 3
+        ProductTypeId = 3,
+        DateStocked = new (2023, 3, 15)
     },
     new Product()
     {
         Name = "Holly and Phoenix Feather Wand",
         Price = 72.00M,
         Available = true,
-        ProductTypeId = 4
+        ProductTypeId = 4,
+        DateStocked = new (2023, 4, 15)
     },
     new Product()
     {
         Name = "Yew and Phoenix Feather Wand",
         Price = 13.00M,
         Available = true,
-        ProductTypeId = 4
+        ProductTypeId = 4,
+        DateStocked = new (2023, 5, 15)
     },
 };
 
@@ -85,14 +93,15 @@ string menuOptions = (@"Make a Selection:
                         3. Delete a Product
                         4. Update Product Details
                         5. Search Products
-                        6. Exit
+                        6. View All Available Products
+                        7. Exit
                         ");
 
 
 
 //^ FUNCTIONALITY to make the menu interactive
 string choice = null; //this initalizes the choice variable as null
-while (choice != "6") // The choice is currently null. When the user selects an option we want to keep running the below until a valid option is chosen. So WHILE the user entered something other than "5" on his keyboard...
+while (choice != "7") // The choice is currently null. When the user selects an option we want to keep running the below until a valid option is chosen. So WHILE the user entered something other than "5" on his keyboard...
 {
     //^ DISPLAY the menu options to the user with WriteLine...
     Console.WriteLine(menuOptions);
@@ -127,6 +136,11 @@ while (choice != "6") // The choice is currently null. When the user selects an 
     }
     else if (choice == "6")
     {
+        // throw new NotImplementedException();
+        ViewAvailableProducts();
+    }
+    else if (choice == "7")
+    {
         Console.WriteLine("Thanks for visiting. Mischief managed, and lumos on your adventures!");
     }
 
@@ -141,8 +155,8 @@ void ListProducts()
     for (int i = 0; i < products.Count; i++)
     {
         string availability = products[i].Available ? "Yes" : "No"; //this is optional because I wanted to display "Yes" instead of true to the user, or "No" instead of false
-//! I don't understand exactly how this works....FirstOrDefault???
-// I know this is a new instance of a ProductType
+                                                                    //! I don't understand exactly how this works....FirstOrDefault???
+                                                                    // I know this is a new instance of a ProductType
         ProductType productType = productTypes.FirstOrDefault(type => type.Id == products[i].ProductTypeId);
         string productTypeName = productType != null ? productType.Name : "Unknown";
         //^DISPLAY product details with WriteLine
@@ -151,6 +165,8 @@ void ListProducts()
             $"Price: ${products[i].Price}\n" +
             $"Available: {availability}\n" +
             $"Type: {productTypeName}\n" +
+            $"Days on the Shelf: {products[i].DaysOnShelf}\n" +
+            $"Seconds on the Shelf: {products[i].SecondsOnShelf}\n" +
             "");
     }
 }
@@ -209,7 +225,7 @@ void DeleteProduct()
 
     //^ READ and STORE the user's input
     int userDeleteSelection = int.Parse(Console.ReadLine()) - 1; //minus one because if the user selects option 5, that is actually index 4....so subtract 1 from the 5 give by the user and we get index 4 to delete
-        
+
     if (userDeleteSelection >= 0 && userDeleteSelection < products.Count)
     {
         Console.WriteLine($"You chose to delete {products[userDeleteSelection].Name}");
@@ -234,16 +250,16 @@ void UpdateProduct()
         Console.WriteLine($"{i + 1}. {products[i].Name}");
     }
     //^ READ and STORE the user's input
-    int userUpdateSelection = int.Parse(Console.ReadLine())-1;
+    int userUpdateSelection = int.Parse(Console.ReadLine()) - 1;
     // int userSelectedIndex = userUpdateSelection - 1; //minus one because if the user selects option 5, that is actually index 4....so subtract 1 from the 5 give by the user and we get index 4 to update
     // Console.WriteLine($"user's actual number {userUpdateSelection}"); //testing
     // Console.WriteLine($"minus one to show index {userSelectedIndex}"); //testing
-    
+
 
     //^ CREATE a new Product-type variable to hold the chosen product
     //! this is what gives us access to the particular product chosen by the user, when we store the FirstOrDefault's method results in the chosenProduct as a whole product
     //? How? We use FirstOrDefault method to LOCATE the product by productId
-    
+
     Product chosenProduct = products.FirstOrDefault(p => p.ProductTypeId == userUpdateSelection);
     Console.WriteLine($"Your product name is currently {products[userUpdateSelection].Name}. Update the name below: ");
     string userUpdatedName = Console.ReadLine().Trim();
@@ -252,7 +268,7 @@ void UpdateProduct()
     Console.WriteLine($"Your product price is currently {products[userUpdateSelection].Price}. Update the price below: ");
     //^ READ and store the user's response - remember to parse
     decimal userUpdatedPrice = decimal.Parse(Console.ReadLine().Trim());
-    
+
     //^ DISPLAY message to the user...
     Console.WriteLine($@"Choose a number 1 - 4 to update the type of product):" + "\n" +
     "1. Apparel" + "\n" +
@@ -262,52 +278,95 @@ void UpdateProduct()
     );
     //^ READ and store the user's response
     int userUpdatedType = int.Parse(Console.ReadLine().Trim());
-    
+
     chosenProduct.Name = userUpdatedName;
     chosenProduct.Price = userUpdatedPrice;
     chosenProduct.ProductTypeId = userUpdatedType;
 }
 
 
-void SearchProduct() 
+// These are good use cases for Where (to filter the products by ProductTypeId) and Select (to turn the List of ProductTypes into strings to display them).
+void SearchProduct()
 {
-    //^ DISPLAY message to the user...
-    Console.Clear();
-    Console.WriteLine($"Look up a product by Type. Choose a number below:"
-    + "\n" +
-    "1. Apparel" + "\n" +
-    "2. Potions" + "\n" +
-    "3. Enchanted Objects" + "\n" +
-    "4. Wands"
-    );
-    int userInputNumber = int.Parse(Console.ReadLine());
-
-    //^ Search the products List by productTypeId (Compare user input to existing list)
-    List<Product> userMatchedProduct = new List<Product>();
-    foreach (Product product in products)
     {
-        if(product.ProductTypeId == (userInputNumber))
+        //^ DISPLAY message to the user...
+        Console.Clear();
+        // Console.WriteLine($"Look up a product by Type. Choose a number below:");
+        // List<ProductType> listofTypes = productTypes.Select(t => t.Name).ToList();
+        // foreach (var productType in listofTypes)
+        // {
+        //     Console.WriteLine(productType);
+        // }
+
+
+        Console.WriteLine($"Look up a product by Type. Choose a number below:"
+        + "\n" +
+        "1. Apparel" + "\n" +
+        "2. Potions" + "\n" +
+        "3. Enchanted Objects" + "\n" +
+        "4. Wands"
+        );
+
+
+        int userInputNumber = int.Parse(Console.ReadLine());
+        //Iterate over the list of products and display each product type
+        // define a new List to hold the product types
+        var userMatchedProducts = products.Where(product => product.ProductTypeId == userInputNumber).ToList();
+
+        Console.WriteLine($@"Here are the matching products:");
+        foreach (Product product in userMatchedProducts)
         {
-            userMatchedProduct.Add(product); //add the matched product to the new List
+            Console.WriteLine(product.Name);
         }
-    }
-    Console.WriteLine($@"Here are the matching products:");
-    foreach (Product product in userMatchedProduct)
+    };
+}
+
+//^BELOW IS THE ORIGINAL SearchProduct code
+// void SearchProduct() 
+// {
+//     //^ DISPLAY message to the user...
+//     Console.Clear();
+//     Console.WriteLine($"Look up a product by Type. Choose a number below:"
+//     + "\n" +
+//     "1. Apparel" + "\n" +
+//     "2. Potions" + "\n" +
+//     "3. Enchanted Objects" + "\n" +
+//     "4. Wands"
+//     );
+//     int userInputNumber = int.Parse(Console.ReadLine());
+
+//     //^ Search the products List by productTypeId (Compare user input to existing list)
+//     List<Product> userMatchedProduct = new List<Product>();
+//     foreach (Product product in products)
+//     {
+//         if(product.ProductTypeId == (userInputNumber))
+//         {
+//             userMatchedProduct.Add(product); //add the matched product to the new List
+//         }
+//     }
+//     Console.WriteLine($@"Here are the matching products:");
+//     foreach (Product product in userMatchedProduct)
+//     {
+//      Console.WriteLine(product.Name);
+//     }
+// };
+
+void ViewAvailableProducts()
+{
+    Console.Clear();
+    List<Product> unsoldProducts = products.Where(p => p.Available).ToList();
+    
+    foreach (Product product in unsoldProducts)
     {
-     Console.WriteLine(product.Name);
+        Console.WriteLine(product);
     }
-
-
-    
 };
-
-    
 
 
 
 
 
 //* LEARNING FirstOrDefault Method
-    //so use FirstOrDefault method on the products list and run an anonymous function on that method that takes (in this case) the property Id and FINDS a product Id among all products that matches the Id we gave it. Then it updates a Product-type variable that references the user's chosen product.
+//so use FirstOrDefault method on the products list and run an anonymous function on that method that takes (in this case) the property Id and FINDS a product Id among all products that matches the Id we gave it. Then it updates a Product-type variable that references the user's chosen product.
 
-    // So the FirstOrDefault method is looking for whatever product property we tell it and UPDATES that property with the user's chosen product(index). That 
+// So the FirstOrDefault method is looking for whatever product property we tell it and UPDATES that property with the user's chosen product(index). That 
